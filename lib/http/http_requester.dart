51e2114ba334126d2f5012a21ee38a5d58acda92
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-06-25 19:25:42 +0200

    Added authentication feature

--- lib/http/http_requester.dart
+++ lib/http/http_requester.dart
@@ -5,7 +5,8 @@ import 'package:http/http.dart' as http;
 class HTTPRequester {
   late final Request _request;
   late Uri _requestCodedIntoURI;
-  late http.Response _response;
+  late http.Response _response; 
+  static String? _authenticationToken;
 
   static Future<Map<String, dynamic>> get(Request request) async {
     HTTPRequester httpRequester = HTTPRequester._internal(request);
@@ -17,11 +18,16 @@ class HTTPRequester {
     return httpRequester._makeGenericRequest(httpRequester._makePOSTRequest);
   }
 
+  static set authenticationToken(String token){
+    _authenticationToken = token;
+  }
+
   HTTPRequester._internal(this._request);
 
-  Future<Map<String, dynamic>> _makeGenericRequest(Future<http.Response> Function() makeSpecificRequest) async {
-    _requestCodedIntoURI = _getRequestCodedIntoURI();
-    _response = await makeSpecificRequest();
+  Future<Map<String, dynamic>> _makeGenericRequest(Future<void> Function() executeSpecificRequest) async {
+    _authenticateRequest();
+    _setRequestCodedIntoURI();
+    await executeSpecificRequest();
     if (_responseSuccess()) {
       return _getResponseDecoded();
     } else {
@@ -29,23 +35,40 @@ class HTTPRequester {
     }
   }
 
-  Future<http.Response> _makeGETRequest() {
-    return http.get(_requestCodedIntoURI,headers: _request.headers);
+  Future<void> _makeGETRequest() async{
+    _response =  await http.get(_requestCodedIntoURI,headers: _request.headers);
   }
 
-  Future<http.Response> _makePOSTRequest() {
-    return http.post(_requestCodedIntoURI,headers: _request.headers, body: _request.body);
+  Future<void> _makePOSTRequest()async {
+    _response = await http.post(_requestCodedIntoURI,headers: _request.headers, body:_request.body);
   }
-
   bool _responseSuccess() {
     return _response.statusCode == 200;
   }
 
-  Uri _getRequestCodedIntoURI() {
-    return Uri.http(_request.url, _request.unencodedPath, _request.queryParameters);
-  }
-
   Map<String, dynamic> _getResponseDecoded() {
     return convert.jsonDecode(_response.body) as Map<String, dynamic>;
+  }
+  void _setRequestCodedIntoURI() {
+    _requestCodedIntoURI =  Uri.http(_request.url, _request.unencodedPath, _request.queryParameters);
+  }
+
+  void _authenticateRequest(){
+    if(_authenticationToken == null) return;
+    if(_requestContainsHeaders()){
+      _addAuthenticationTokenToRequestHeaders();
+    }else{
+      _createHeadersToRequestWithAuthenticationToken();
+    }
+  }
+
+  bool _requestContainsHeaders(){
+    return _request.headers != null;
+  }
+  void _addAuthenticationTokenToRequestHeaders(){
+      _request.headers!.addAll({"Authorization": "Bearer $_authenticationToken"});
+  }
+  void _createHeadersToRequestWithAuthenticationToken(){
+      _request.headers = {"Authorization": "Bearer $_authenticationToken"};
   }
 }
