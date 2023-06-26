8f42603362627bbec5ece7222472b84239406ac4
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-06-26 20:55:06 +0200

    New class for popup alerts

---
+++ lib/popup_alert_displayer/popup_alert_displayer.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class PopUpAlertDisplayer{  
+  late final BuildContext _context;
+  late final String _title;
+  late final String _message;
+
+  static showAlert(BuildContext context, String title,String message){
+    PopUpAlertDisplayer displayer = PopUpAlertDisplayer._internal(context, title, message);
+    displayer._showAlert();
+  }
+
+  PopUpAlertDisplayer._internal(this._context,this._title,this._message);
+
+  Future<void> _showAlert() async {
+    return showDialog<void>(
+      context: _context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return _getAlertDialog();
+      },
+    );
+  }
+
+  Widget _getAlertDialog(){
+    return AlertDialog(
+      title: Text(_title),
+      content: SingleChildScrollView(
+        child: Text(_message),
+      ),
+      actions: <Widget>[
+        _getOkButton(),
+      ],
+    );
+  }
+
+  Widget _getOkButton(){
+    return TextButton(
+      child: const Text('Ok'),
+      onPressed: () {
+        Navigator.of(_context).pop();
+      },
+    );
+  }
+}  
