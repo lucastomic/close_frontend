dad90a44476a4e94b1b50450e714d3b166cfea1e
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-07-15 17:43:50 +0200

    NavigationBar UI modified with external package

--- lib/widgets/main_screen/customized_bottom_navigation_bar.dart
+++ lib/widgets/main_screen/customized_bottom_navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class CustomizedNaviagtionBar extends StatelessWidget {
   final Function(int) _onItemTapped;
@@ -10,16 +11,27 @@ class CustomizedNaviagtionBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      showUnselectedLabels: false,
-      showSelectedLabels: false,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "settings"),
-        BottomNavigationBarItem(icon: Icon(Icons.gps_fixed), label: "close"),
-        BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: "settings")
-      ],
-      onTap: _onItemTapped,
-      currentIndex: _selectedIndex,
-    );
+    return  GNav(
+      onTabChange: _onItemTapped,
+      selectedIndex: _selectedIndex,
+      color: Theme.of(context).colorScheme.secondary, // unselected icon color
+      activeColor: Theme.of(context).colorScheme.primary, 
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      iconSize: 28,
+      tabs: const [
+        GButton(
+          icon: Icons.settings,
+          text: 'Ajustes',
+        ),
+        GButton(
+          icon: Icons.gps_fixed,
+          text: 'Cerca',
+        ),
+        GButton(
+          icon: Icons.account_circle,
+          text: 'Perfil',
+        ),
+      ]
+     );
   }
 }
