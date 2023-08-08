751c38ef96f25e1a7086f2c4bb476102b748b96c
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-08-08 12:07:01 +0200

    Improved redeability

--- lib/widgets/util_widgets/social_networks_list.dart
+++ lib/widgets/util_widgets/social_networks_list.dart
@@ -4,12 +4,20 @@ import 'package:close_frontend/extensions/string_extensions.dart';
 import 'package:flutter/material.dart';
 
 class SocialNetworksList extends StatelessWidget {
-  final Map<SocialNetwork,String> _socialNetworks;
-  final int maxSocialNetworksDisplayed = 3;
+  final Map<SocialNetwork,String> _socialNetworksToUsername;
+  final int _maxSocialNetworksDisplayed;
   final double _logoSize;
   final double _fontSize;
-  const SocialNetworksList(this._socialNetworks, {double logoSize = 18, double fontSize = 14}):
-  _logoSize = logoSize, _fontSize =fontSize;
+
+  const SocialNetworksList(
+    this._socialNetworksToUsername, 
+    {
+      double logoSize = 18, 
+      double fontSize = 14, 
+      int maxSocialNetworksDisplayed = 3
+    }
+  ):
+  _logoSize = logoSize, _fontSize =fontSize, _maxSocialNetworksDisplayed = maxSocialNetworksDisplayed;
 
   @override
   Widget build(BuildContext context) {
@@ -18,27 +26,41 @@ class SocialNetworksList extends StatelessWidget {
       shrinkWrap: true,
       itemCount:  _getNumberOfSocialNetworksDisplayed(),
       itemBuilder: (_, int index){
-        SocialNetwork key = _socialNetworks.keys.elementAt(index);
+        SocialNetwork socialNetwork = _socialNetworksToUsername.keys.elementAt(index);
         return Padding(
           padding: const EdgeInsets.only(bottom: 4.0),
-          child: Row(
-            children: [
-              SizedBox(width:_logoSize, child: key.logo),
-              const SizedBox(width: 7,),
-              Text(
-                _socialNetworks[key]!.asUsername(),
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: _fontSize),  
-              ),
-            ],
-          ),
+          child: _SocialNetworkRow(socialNetwork, _socialNetworksToUsername[socialNetwork]!, _logoSize, _fontSize),
         );
       }
     );
   }
 
   int _getNumberOfSocialNetworksDisplayed(){
-    return min(maxSocialNetworksDisplayed, _socialNetworks.length);
+    return min(_maxSocialNetworksDisplayed, _socialNetworksToUsername.length);
   }
 
+}
+
+
+class _SocialNetworkRow extends StatelessWidget {
+  final SocialNetwork _socialNetwork;
+  final double _logoSize;
+  final String _username;
+  final double _fontSize;
+  const _SocialNetworkRow(this._socialNetwork, this._username, this._logoSize, this._fontSize);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(width:_logoSize, child: _socialNetwork.logo),
+        const SizedBox(width: 7,),
+        Text(
+          _username.asUsername(),
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: _fontSize),  
+        ),
+      ],
+    );
+  }
 }
