0a0874785a0b2bd28e7108bd9f1d02cb3d156033
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-08-13 17:31:58 +0200

    Closing stream when dispose and redeability

--- lib/widgets/chat_screen/chat_rendering.dart
+++ lib/widgets/chat_screen/chat_rendering.dart
@@ -1,38 +1,48 @@
+import 'package:close_frontend/domain/chat/chat.dart';
 import 'package:close_frontend/domain/user/user.dart';
 import 'package:close_frontend/services/chat_service/chat_service_port.dart';
 import 'package:close_frontend/widgets/chat_screen/lazy_loading_messages_list/messages_list_with_lazy_loading.dart';
 import 'package:flutter/material.dart';
 
-class ChatRendering extends StatefulWidget {
-  IChatService _chatService;
-  User _receiver;
+class ChatRenderer extends StatefulWidget {
+  final IChatService _chatService;
+  final User _externalUser;
 
-  ChatRendering({required IChatService chatService, required User receiver}):
+  const ChatRenderer({required IChatService chatService, required User externalUser}):
   _chatService = chatService,
-  _receiver = receiver;
+  _externalUser = externalUser;
 
   @override
-  State<ChatRendering> createState() => _ChatRenderingState();
+  State<ChatRenderer> createState() => _ChatRendererState();
 }
 
-class _ChatRenderingState extends State<ChatRendering> {
+class _ChatRendererState extends State<ChatRenderer> {
+
+  @override
+  void dispose() {
+    widget._chatService.closeChatStream();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future:widget._chatService.getChat(widget._receiver),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            return StreamBuilder(
-              initialData: snapshot.data,
-              stream: widget._chatService.getChatStream(context),
-              builder: (context, snapshotStream) {
-                return MessagesListWithLazyLoading(snapshotStream.data!);
-              },
-            );
-          }else{
-            return Expanded(child: Container(),);
-          }
-        },
+        future:widget._chatService.getChat(widget._externalUser),
+        builder:_buildChatFromFutureSnapshot,
     );
+  }
+
+  Widget _buildChatFromFutureSnapshot(BuildContext context, AsyncSnapshot snapshot){
+    if(snapshot.hasData){
+      return StreamBuilder<Chat>(
+        initialData: snapshot.data,
+        stream: widget._chatService.getChatStream(context),
+        builder: (context, snapshotStream) {
+          return MessagesListWithLazyLoading(snapshotStream.data!);
+        },
+      );
+    }else{
+      return Expanded(child: Container(),);
+    }
   }
 }
